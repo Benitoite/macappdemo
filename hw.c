c76f3c8742f273bff2aae3e2c9f1e11478f5e30b
Richard E Barber <kd6kxr@gmail.com>
2023-09-10 18:33:31 -0700

    Update hw.c

--- hw.c
+++ hw.c
@@ -7,14 +7,14 @@ int main()
 {
     char * cmnd = NULL;
     cmnd = malloc(2048);
-    strcpy(cmnd, "osascript ");
+    strcpy(cmnd, "osascript ");           // start building command
     char path[1024];
     uint32_t size = sizeof(path);
-    _NSGetExecutablePath(path, &size);
-    int len = strlen(path);
-    path[len-3] = '\0';
+    _NSGetExecutablePath(path, &size);    // get the POSIX path of the "hw" executable
+    int len = strlen(path);               // get the length of the path
+    path[len-3] = '\0';                   // remove the "/hw" from the end of the path
     strcat(cmnd, path);
     strcat(cmnd, "/../Resources/Scripts/hi.scpt");
     const char * finalstr = cmnd;
-    return(system(finalstr));
+    return(system(finalstr));             // issue command returning any error conditions
 }
